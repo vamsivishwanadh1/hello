fe40ed0e98c1002730aa11f1670ba218540bb01d
vamsivishwanadh1 <vamsivishwanadh@msitprogram.net>
2018-08-03 16:04:19 +0530

    " -> 0 of 4 passed. pylint: 2.22/10 "

--- cspp1/p1/vowels_counter.py
+++ cspp1/p1/vowels_counter.py
@@ -1,10 +1,9 @@
-#Assume Z is a string of lower case characters.
 def main():
-Z = input()
-VOWELS = 0
-for char in V:
-    if char in 'aeiou':
-        VOWELS += 1
-print(VOWELS)	
-if __name__== "__main__":
+    Z = input()
+    VOWELS = 0
+    for char in Z:
+        if char in 'aeiou':
+            VOWELS += 1
+    print(VOWELS)	
+if __name__ == "__main__":
 	main()
