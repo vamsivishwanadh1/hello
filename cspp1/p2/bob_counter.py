72ec71b6bdac115a6196102ebb886927fe21a28c
vamsivishwanadh1 <vamsivishwanadh@msitprogram.net>
2018-08-03 17:11:26 +0530

    " -> 2 of 5 passed. pylint: -7.14/10 "

--- cspp1/p2/bob_counter.py
+++ cspp1/p2/bob_counter.py
@@ -5,14 +5,14 @@ Write a program that prints the number of times the string 'bob' occurs in s. Fo
 Number of times bob occurs is: 2'''
 
 def main():
-	s = raw_input()
+	
 	# the input string is in s
 	# remove pass and start your code here
-"'Take a variable V'"
-V = str(input())
-X = 'bob'
-T = V.count(X)
-print(T)
+        '''Take a variable V'''
+        V = str(input())
+        X = 'bob'
+        T = V.count(X)
+        print(T)
 
 if __name__== "__main__":
 	main()
