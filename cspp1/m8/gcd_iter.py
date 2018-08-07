65f3b277df6c5562ac0933953157442566743d16
vamsivishwanadh1 <vamsivishwanadh@msitprogram.net>
2018-08-07 14:28:18 +0530

    gcd_iter.py

---
+++ cspp1/m8/gcd_iter.py
@@ -0,0 +1,31 @@
+# Exercise: GCDIter
+# Write a Python function, gcdIter(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.
+
+# This function takes in two numbers and returns one number.
+
+
+def gcdIter(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    if a > b:
+       small_value = b
+    else:
+        small_value = a
+    while small_value >= 1:
+        if a%small_value == 0 and b%small_value == 0:
+            break
+        small_value -= 1
+    return small_value
+
+
+
+def main():
+    data = input()
+    data = data.split()
+    print(gcdIter(int(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
